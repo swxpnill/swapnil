310a4ff8ed3dd546a250a67752edbebad675dcf3
Swapnil <119793591+swxpnill@users.noreply.github.com>
2024-12-25 11:41:16 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -8,7 +8,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/swxpnill",
   resume:
-    "https://drive.google.com/file/d/1bx6FvpCghJ1XnxqHmb60-loddYOFsPGm/view?usp=drive_link",
+    "https://drive.google.com/file/d/1MGRjATI5RidhmJwy7zbjoJ9_QPd4iAwt/view?usp=drive_link",
   linkedin: "https://www.linkedin.com/in/swapnilpandit16/",
   // twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/swxpnill/",
